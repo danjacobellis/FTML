7f03a25f0d42acddb1bf217396e17d6160bbe761
Dan <danjacobellis@gmail.com>
2022-09-08 12:06:26 -0500

    build

--- build.py
+++ build.py
@@ -13,5 +13,8 @@ def build():
     result = run("jupyter-nbconvert --to slides critique_paper2.ipynb --output docs\critique_paper2 --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True)
     print(result.stdout.decode("utf-8"))
     
+    result = run("jupyter-nbconvert --to slides present_paper10.ipynb --output docs\present_paper10 --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True)
+    print(result.stdout.decode("utf-8"))
+    
 if __name__ == '__main__':
     build()
--- index.md
+++ index.md
@@ -3,4 +3,5 @@
 ```{toctree}
 :maxdepth: 2
 critique_paper2
+present_paper10
 ```
