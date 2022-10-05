2c7c651079fc9de2309b5318d30b02d0f3bca972
Dan <danjacobellis@gmail.com>
2022-10-05 10:43:59 -0500

    slides

--- build.py
+++ build.py
@@ -16,5 +16,8 @@ def build():
     result = run("jupyter-nbconvert --to slides present_paper10.ipynb --output docs\present_paper10 --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True)
     print(result.stdout.decode("utf-8"))
     
+    result = run("jupyter-nbconvert --to slides critique_50y_fairness.ipynb --output docs\critique_50y_fairness --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True)
+    print(result.stdout.decode("utf-8"))
+    
 if __name__ == '__main__':
     build()
