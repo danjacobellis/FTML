c633bb2a414b9869d95fbc6df06dd92c9db1e19b
Dan Jacobellis <danjacobellis@utexas.edu>
2022-10-10 08:54:49 -0500

    build

--- build.py
+++ build.py
@@ -6,17 +6,17 @@ def build():
     
     if exists("docs"):
         rmtree("docs")
-    result = run("sphinx-build . docs",capture_output=True)
+    result = run(["sphinx-build . docs"],capture_output=True, shell=True)
     print(result.stdout.decode("utf-8"))
     Path("docs/.nojekyll").touch()
     
-    result = run("jupyter-nbconvert --to slides critique_paper2.ipynb --output docs\critique_paper2 --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True)
+    result = run("jupyter-nbconvert --to slides critique_paper2.ipynb --output docs/critique_paper2 --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True, shell=True)
     print(result.stdout.decode("utf-8"))
     
-    result = run("jupyter-nbconvert --to slides present_paper10.ipynb --output docs\present_paper10 --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True)
+    result = run("jupyter-nbconvert --to slides present_paper10.ipynb --output docs/present_paper10 --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True, shell=True)
     print(result.stdout.decode("utf-8"))
     
-    result = run("jupyter-nbconvert --to slides critique_50y_fairness.ipynb --output docs\critique_50y_fairness --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True)
+    result = run("jupyter-nbconvert --to slides critique_50y_fairness.ipynb --output docs/critique_50y_fairness --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='remove-nb-cell'",capture_output=True, shell=True)
     print(result.stdout.decode("utf-8"))
     
 if __name__ == '__main__':
