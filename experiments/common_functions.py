4a52ce7ba3d8b8c7f0d9081f392172b72c281a72
Dan Jacobellis <danjacobellis@utexas.edu>
2022-12-07 14:09:55 -0600

    fgsm

---
+++ experiments/common_functions.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import tensorflow_io as tfio
+import numpy as np
+import PIL
+
+# Helper function to preprocess the image so that it can be inputted in MobileNetV2
+def preprocess(image):
+  image = tf.cast(image, tf.float32)
+  image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
+  image = image[None, ...]
+  return image
+
+# Helper function to extract labels from probability vector
+def get_imagenet_label(probs):
+  return decode_predictions(probs, top=1)[0][0]
+
+def create_adversarial_pattern(input_image, input_label):
+
+  return signed_grad
+    
+def show(img_rgb):
+    return PIL.Image.fromarray(np.squeeze(np.array(img_rgb)))
+
+def show_perturbation(signed_grad):
+    p = 127.5*np.squeeze(np.array(signed_grad+1))
+    return PIL.Image.fromarray(p.astype('uint8'))
+
+decode_predictions = tf.keras.applications.mobilenet_v2.decode_predictions
+
+def png_to_rgb(png_filename):
+    img_png = tf.io.read_file('imagenet_crops/guitar_01.png')
+    return tf.io.decode_png(img_png)
+
+def fgsm(img_rgb):
+    pretrained_model = tf.keras.applications.MobileNetV2(include_top=True,weights='imagenet')
+    pretrained_model.trainable = False
+    
+    input_image = preprocess(img_rgb)
+    input_probs = pretrained_model.predict(input_image)
+    input_label = get_imagenet_label(input_probs)
+    
+    loss_object = tf.keras.losses.CategoricalCrossentropy()
+    
+    with tf.GradientTape() as tape:
+        tape.watch(input_image)
+        prediction = pretrained_model(input_image)
+        loss = loss_object(input_probs, prediction)
+    # Get the gradients of the loss w.r.t to the input image.
+    gradient = tape.gradient(loss, input_image)
+    # Get the sign of the gradients to create the perturbation
+    signed_grad = tf.sign(gradient)
+    
+    return input_label, signed_grad
